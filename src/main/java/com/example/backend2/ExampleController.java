2e64eda21e9c61e8661f59262b6b4d743f3c65f1
Hamlet Lee <lisn@rd.netease.com>
2019-11-25 17:36:43 +0800

    MOD: 增加了/api/new 接口

--- src/main/java/com/example/backend2/ExampleController.java
+++ src/main/java/com/example/backend2/ExampleController.java
@@ -1,13 +1,30 @@
 package com.example.backend2;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class ExampleController {
+    private List<Item> list = new ArrayList<>();
+    private long maxId = 0;
+
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public synchronized void add(@RequestBody String body) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Item item = objectMapper.readValue(body, Item.class);
+        item.setId(++maxId);
+        list.add(item);
+    }
+
     @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "hi！";
---
+++ src/main/java/com/example/backend2/Item.java
@@ -0,0 +1,31 @@
+package com.example.backend2;
+
+public class Item {
+    private long id;
+    private String name;
+    private String tel;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
