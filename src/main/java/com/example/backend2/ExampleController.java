563f4b6477ee2155f4f12e27827c1a9bf7263d28
Hamlet Lee <lisn@rd.netease.com>
2019-12-03 12:32:30 +0800

    ADD: /api/userinfo

--- src/main/java/com/example/backend2/ExampleController.java
+++ src/main/java/com/example/backend2/ExampleController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -23,6 +25,14 @@ public class ExampleController {
         Item item = objectMapper.readValue(body, Item.class);
         item.setId(++maxId);
         list.add(item);
+    }
+
+    @RequestMapping(path = "/userinfo", method = RequestMethod.POST)
+    public synchronized Object userinfo(@RequestBody String body, HttpServletRequest req) throws JsonProcessingException {
+        Map<String,Object> retMap = new HashMap<>();
+        String ldap = req.getHeader("X-LDAP");
+        retMap.put("name", ldap);
+        return retMap;
     }
 
     @RequestMapping(path = "/hello", method = RequestMethod.GET)
